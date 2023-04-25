cba55b9fde357961ffcfbd069a17df7495452a64
azanniel <leo.azannielttt@gmail.com>
2023-04-24 22:01:21 -0400

    ✨ add list empty component for flatlists

--- src/screens/Groups/index.tsx
+++ src/screens/Groups/index.tsx
@@ -3,15 +3,38 @@ import { Highlight } from '@components/Highlight'
 
 import { GroupsContainer } from './styles'
 import { GroupCard } from '@components/GroupCard'
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+import { ListEmpty } from '@components/ListEmpty'
 
 export function Groups() {
+  const [groups] = useState<string[]>([
+    'Galera da Rocketseat',
+    'Galera da Creathus',
+    'Galera da ATX',
+  ])
+
   return (
     <GroupsContainer>
       <Header />
 
       <Highlight title="Turmas" subtitle="Jogue com a sua turma" />
 
-      <GroupCard title="Galera do Ignite" />
+      <FlatList
+        data={groups}
+        keyExtractor={(item) => item}
+        renderItem={({ item }) => {
+          return <GroupCard title={item} />
+        }}
+        contentContainerStyle={
+          groups.length === 0 && {
+            flex: 1,
+          }
+        }
+        ListEmptyComponent={() => (
+          <ListEmpty message="Que tal cadastrar a primeira turma?" />
+        )}
+      />
     </GroupsContainer>
   )
 }
