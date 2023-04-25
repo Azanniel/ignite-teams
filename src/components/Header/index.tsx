7e242816692263aff4fb92dbb34bbb6820dc50f4
azanniel <leo.azannielttt@gmail.com>
2023-04-24 21:28:22 -0400

    📦️ add highlight component
    add components, phosphor icon package and improve header component

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,10 +1,20 @@
-import { HeaderContainer, Logo } from './styles'
+import { HeaderContainer, BackButton, BackIcon, Logo } from './styles'
 
 import logo from '@assets/logo.png'
 
-export function Header() {
+type HeaderProps = {
+  showBackButton?: boolean
+}
+
+export function Header({ showBackButton = false }: HeaderProps) {
   return (
     <HeaderContainer>
+      {showBackButton && (
+        <BackButton>
+          <BackIcon />
+        </BackButton>
+      )}
+
       <Logo source={logo} />
     </HeaderContainer>
   )
