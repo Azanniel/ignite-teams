e7ae66805d32d289b5acc710503d6814faeb4ab6
azanniel <leo.azannielttt@gmail.com>
2023-04-26 23:18:37 -0400

    🏷️ add players screen and horizontal flatlist

--- src/screens/Players/index.tsx
+++ src/screens/Players/index.tsx
@@ -1,11 +1,18 @@
+import { useState } from 'react'
+import { FlatList } from 'react-native'
+
 import { Header } from '@components/Header'
 import { Highlight } from '@components/Highlight'
 import { Input } from '@components/Input'
 import { ButtonIcon } from '@components/ButtonIcon'
+import { Filter } from '@components/Filter'
 
-import { Form, PlayersContainer } from './styles'
+import { PlayersContainer, Form, HeaderList, NumbersOfPlayers } from './styles'
 
 export function Players() {
+  const [team, setTeam] = useState('Time A')
+  const [players] = useState(['Leandro', 'Azanniel', 'Lima'])
+
   return (
     <PlayersContainer>
       <Header showBackButton />
@@ -19,6 +26,25 @@ export function Players() {
         <Input placeholder="Nome da pessoa" autoCorrect={false} />
         <ButtonIcon icon="add" />
       </Form>
+
+      <HeaderList>
+        <FlatList
+          data={['Time A', 'Time B']}
+          keyExtractor={(item) => item}
+          renderItem={({ item }) => {
+            return (
+              <Filter
+                title={item}
+                isActive={team === item}
+                onPress={() => setTeam(item)}
+              />
+            )
+          }}
+          horizontal
+        />
+
+        <NumbersOfPlayers>{players.length}</NumbersOfPlayers>
+      </HeaderList>
     </PlayersContainer>
   )
 }
